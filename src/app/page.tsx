c3e4e5b016f48238032925b9380c79e503f5be8f
Rachel Tomasetti <rmt108@miami.edu>
2025-04-17 05:56:49 -0400

    lower

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   return (
     <main className="min-h-screen p-4 sm:p-8">
       <div className="flex flex-col gap-4 mb-6">
-        <p>Hi, welcome.</p>
-        <p>I am immensely curious about the power of the mind.</p>
-        <p>Here you'll find what's been on my mind.</p>
+        <p>hi, welcome.</p>
+        <p>i am immensely curious about the power of the mind.</p>
+        <p>here you'll find what's been on my mind.</p>
         <ul className="list-disc pl-6 space-y-2">
           <li>
             <a href="/ki" className="hover:font-bold transition-all">
-              Ki
+              ki
             </a>
           </li>
         </ul>
