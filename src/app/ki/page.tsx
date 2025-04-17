3191bdbc44886fb63b36e568b82cf22266be72bd
Rachel Tomasetti <rmt108@miami.edu>
2025-04-17 05:43:08 -0400

    update4-17

---
+++ src/app/ki/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Ki() {
+  return (
+    <main className="min-h-screen p-4 sm:p-8">
+      <p>I don't know where this is going. so let's have some fun....</p>
+    </main>
+  );
+}
