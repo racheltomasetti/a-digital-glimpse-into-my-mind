11ad2492f0921a485359b52079a1190737d641a8
Rachel Tomasetti <rmt108@miami.edu>
2025-05-13 12:03:02 -0400

    removed flowers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,22 +21,6 @@ export default function Home() {
             className="w-24 sm:w-32 md:w-44 lg:w-56 h-auto z-10"
             priority
           />
-          <Image
-            src="/flower.png"
-            alt="Flower Left"
-            width={64}
-            height={128}
-            className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-1/3 w-14 sm:w-16 md:w-20 lg:w-24 h-auto z-20"
-            priority
-          />
-          <Image
-            src="/flower.png"
-            alt="Flower Right"
-            width={64}
-            height={128}
-            className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/3 w-14 sm:w-16 md:w-20 lg:w-24 h-auto scale-x-[-1] z-20"
-            priority
-          />
         </div>
       </div>
       {/* Content goes here */}
